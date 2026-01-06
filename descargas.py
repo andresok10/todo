6ec27471e8eada8ffa4079d3a28e60a8ed981cc9
andresok10 <davexgarcia@gmail.com>
2026-01-06 06:28:54 -0500

    ok

--- descargas.py
+++ descargas.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, url_for, send_from_directory, current_app
-from yt_dlp import YoutubeDL # pip install yt-dlp
-import os, urllib.request,zipfile ,tarfile, ssl, certifi, shutil
+from yt_dlp import YoutubeDL  # pip install yt-dlp
+import os, urllib.request, zipfile, tarfile, ssl, certifi, shutil
 import os, zipfile, tarfile, urllib.request, shutil, ssl, certifi, platform
 
 app2 = Blueprint("descargas", __name__)
@@ -12,7 +12,9 @@ is_linux = platform.system().lower().startswith("linux")
 print(is_linux)
 if not os.path.exists(f"{ffmpeg_dir}/ffmpeg"):
     print("FFmpeg no encontrado. Descargando...")
-    ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
+    ssl._create_default_https_context = lambda: ssl.create_default_context(
+        cafile=certifi.where()
+    )
     if is_linux:
         url = "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
         archivo = f"{ffmpeg_dir}/ffmpeg.tar.xz"
@@ -29,30 +31,31 @@ if not os.path.exists(f"{ffmpeg_dir}/ffmpeg"):
 
         if os.path.isfile(f"{ffmpeg_dir}/ffmpeg.tar.xz"):
             os.remove(f"{ffmpeg_dir}/ffmpeg.tar.xz")
-        #if os.path.exists(f"{ffmpeg_dir}/ffmpeg-7.0.2-amd64-static"):
+        # if os.path.exists(f"{ffmpeg_dir}/ffmpeg-7.0.2-amd64-static"):
         #    shutil.rmtree(f"{ffmpeg_dir}/ffmpeg-7.0.2-amd64-static")
 
         # Dar permisos de ejecución al binario
         os.chmod(f"{ffmpeg_dir}/ffmpeg/ffmpeg", 0o755)
-        #os.chmod(ffmpeg_bin, 0o755)
-        #os.chmod(ffprobe_bin, 0o755)
+        # os.chmod(ffmpeg_bin, 0o755)
+        # os.chmod(ffprobe_bin, 0o755)
     else:
         raise Exception("❌ Sistema operativo no soportado")
 ##################################################
-#BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "downloads")
+# BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "downloads")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print(BASE_DIR) #/opt/render/project/src/
+print(BASE_DIR)  # /opt/render/project/src/
 os.makedirs(f"{BASE_DIR}/descarga", exist_ok=True)
 
-#FFMPEG_PATH = f"{BASE_DIR}/ffmpeg/bin/ffmpeg.exe" # windows
-FFMPEG_PATH = f"{BASE_DIR}/ffmpeg/ffmpeg" # linux
-print(FFMPEG_PATH) #/opt/render/project/src/ffmpeg/ffmpeg
+# FFMPEG_PATH = f"{BASE_DIR}/ffmpeg/bin/ffmpeg.exe" # windows
+FFMPEG_PATH = f"{BASE_DIR}/ffmpeg/ffmpeg"  # linux
+print(FFMPEG_PATH)  # /opt/render/project/src/ffmpeg/ffmpeg
+
 
 ####
 @app2.route("/descarga_flutter", methods=["POST"])
 def descarga_flutterx():
     carpeta = os.path.join(BASE_DIR, "descarga")
-    #carpeta = "/opt/render/project/src/descarga"
+    # carpeta = "/opt/render/project/src/descarga"
     try:
         data = request.get_json()
         url = data.get("url").split("?")[0]
@@ -68,37 +71,36 @@ def descarga_flutterx():
         counter = 1
         while True:
             file = f"{BASE_DIR}/descarga/{counter}.{extension}"
-            #file = f"{BASE_DIR}/descarga/{counter}"
+            # file = f"{BASE_DIR}/descarga/{counter}"
             if not os.path.exists(file):
                 break
             counter += 1
 
-        #"format": "bestaudio/best" if download_type == "audio" else "best", "bestvideo+bestaudio/best",
+        # "format": "bestaudio/best" if download_type == "audio" else "best", "bestvideo+bestaudio/best",
         if download_type == "audio":
             ydl_opts = {
                 "format": "bestaudio/best",
-                #"outtmpl": file + ".%(ext)s",  # añadir extensión aquí,
+                # "outtmpl": file + ".%(ext)s",  # añadir extensión aquí,
                 "outtmpl": file,  # añadir extensión aquí,
                 "ffmpeg_location": FFMPEG_PATH,
                 "quiet": False,
                 "noplaylist": True,
             }
-        else: # video
+        else:  # video
             ydl_opts = {
                 "outtmpl": file,  # añadir extensión aquí,
-                #"outtmpl": f"{counter}.{extension}",  # añadir extensión aquí,
-                #"outtmpl": file + ".%(ext)s",  # añadir extensión aquí,
-                #"format": "bestvideo+bestaudio/best",
-                #"merge_output_format": extension,  # 🔥 esta línea fuerza la extensión
-                'format': 'bestvideo[ext=webm]+bestaudio[ext=webm]/best',
+                # "outtmpl": f"{counter}.{extension}",  # añadir extensión aquí,
+                # "outtmpl": file + ".%(ext)s",  # añadir extensión aquí,
+                # "format": "bestvideo+bestaudio/best",
+                # "merge_output_format": extension,  # 🔥 esta línea fuerza la extensión
+                "format": "bestvideo[ext=webm]+bestaudio[ext=webm]/best",
                 #'format': 'best',
-                'merge_output_format': extension,
+                "merge_output_format": extension,
                 "ffmpeg_location": FFMPEG_PATH,
-                #"quiet": False,
+                # "quiet": False,
                 "noplaylist": True,
-                'postprocessor_args': ['-strict', '-2'],  # opcional
+                "postprocessor_args": ["-strict", "-2"],  # opcional
                 #'postprocessor_args': ['-c', 'copy', '-strict', '-2']
-
             }
 
         # Descargar archivo
@@ -110,74 +112,109 @@ def descarga_flutterx():
         real_extension = file_basename.split(".")[-1]  # extrae 'webm', 'mp4', etc.
 
         # Nombre base del archivo descargado
-        #file_basename = os.path.basename(file)
-        #file_basename = f"{BASE_DIR}/descarga/{counter}.{extension}"
-        #file_basename = f"{counter}.{extension}"
+        # file_basename = os.path.basename(file)
+        # file_basename = f"{BASE_DIR}/descarga/{counter}.{extension}"
+        # file_basename = f"{counter}.{extension}"
 
         # ✅ Construir URL con HTTPS para evitar el error CLEARTEXT
-        #download_url = url_for("serve_download", file=file_basename, _external=True, _scheme="https")
-        #download_url = url_for("descargax", file=file_basename, _external=True, _scheme="https")
-        download_url = url_for("descargas.serve_download", file=file_basename, _external=True, _scheme="https")
+        # download_url = url_for("serve_download", file=file_basename, _external=True, _scheme="https")
+        # download_url = url_for("descargax", file=file_basename, _external=True, _scheme="https")
+        download_url = url_for(
+            "descargas.serve_download",
+            file=file_basename,
+            _external=True,
+            _scheme="https",
+        )
 
-
-        msgx = f"{download_type.capitalize()} descargado con éxito como {file_basename}."
-        return jsonify({
-            "status": "success",
-            "msg": msgx,
-            "download_url": download_url,
-            "extension": real_extension  # enviamos la extensión real
-        })
+        msgx = (
+            f"{download_type.capitalize()} descargado con éxito como {file_basename}."
+        )
+        return jsonify(
+            {
+                "status": "success",
+                "msg": msgx,
+                "download_url": download_url,
+                "extension": real_extension,  # enviamos la extensión real
+            }
+        )
     except Exception as e:
         print("❌ ERROR:", str(e))
-        return jsonify({"status": "error","msg": f"Error al descargar el archivo: {str(e)}"}), 500
+        return (
+            jsonify(
+                {"status": "error", "msg": f"Error al descargar el archivo: {str(e)}"}
+            ),
+            500,
+        )
 
     finally:
         # Esto se ejecuta siempre, haya error o no
-        #carpeta = os.path.join(BASE_DIR, "descarga")
-        #if os.path.exists(carpeta):
+        # carpeta = os.path.join(BASE_DIR, "descarga")
+        # if os.path.exists(carpeta):
         #    print(f"📂 Contenido actual de: {carpeta}")
         #    for nombre in os.listdir(carpeta):
-                #ruta_completa = os.path.join(carpeta, nombre)
-                #print("   ➜", ruta_completa)
+        # ruta_completa = os.path.join(carpeta, nombre)
+        # print("   ➜", ruta_completa)
         #        print("   ➜", nombre)
-        #else:
+        # else:
         #    print(f"❌ La carpeta {carpeta} no existe.")
         ########################################################
-        #carpeta = "/opt/render/project/src/descarga"
-        #if os.path.exists(carpeta):
+        # carpeta = "/opt/render/project/src/descarga"
+        # if os.path.exists(carpeta):
         #    print(f"📂 Contenido de {carpeta}:")
         #    for archivo in os.listdir(carpeta):
         #        print("   ➜", archivo)
-        #else:
+        # else:
         #    print(f"❌ La carpeta {carpeta} no existe.")
 
-        if os.path.exists(carpeta):
-            current_app.logger.info(f"📂 Contenido actual de {carpeta}:")
-            for archivo in os.listdir(carpeta):
-                current_app.logger.info(f"   ➜ {archivo}")
-                #os.remove(archivo)
-                os.remove(os.path.join(carpeta, archivo))
-                current_app.logger.info(f"   ➜ {archivo}")
-        #else:
+        #if os.path.exists(carpeta):
+        #    current_app.logger.info(f"📂 Contenido actual de {carpeta}:")
+        #    for archivo in os.listdir(carpeta):
+        #        current_app.logger.info(f"   ➜ {archivo}")
+                # os.remove(archivo)
+        #        os.remove(os.path.join(carpeta, archivo))
+        #        current_app.logger.info(f"   ➜ {archivo}")
+        # else:
         #    current_app.logger.info(f"❌ La carpeta {carpeta} no existe.")
+        
+        if os.path.exists(carpeta):
+            archivos = os.listdir(carpeta)
+            if archivos:
+                current_app.logger.info(
+                    f"📂 Contenido actual de {carpeta} antes de limpiar:"
+                )
+                for archivo in archivos:
+                    current_app.logger.info(f"   ➜ {archivo}")
+                    try:
+                        os.remove(os.path.join(carpeta, archivo))
+                        current_app.logger.info(f"      🗑 Eliminado: {archivo}")
+                    except Exception as ex:
+                        current_app.logger.error(
+                            f"      ❌ No se pudo eliminar {archivo}: {str(ex)}"
+                        )
+            else:
+                current_app.logger.info(f"📂 La carpeta {carpeta} está vacía.")
+        else:
+            current_app.logger.info(f"❌ La carpeta {carpeta} no existe.")
+
 
 # Servir correctamente los archivos desde /downloads/
 @app2.route("/descargax/<path:file>")
 def serve_download(file):
-    #Sirve los archivos descargados directamente
+    # Sirve los archivos descargados directamente
     return send_from_directory(f"{BASE_DIR}/descarga", file, as_attachment=True)
-    #return send_from_directory(f"{BASE_DIR}/downloads", os.path.basename(filename), as_attachment=True)
+    # return send_from_directory(f"{BASE_DIR}/downloads", os.path.basename(filename), as_attachment=True)
+
 
 ## Si quieres habilitar descarga directa de archivos:
-#@app.route("/downloads/<path:filename>")
+# @app.route("/downloads/<path:filename>")
 # @app.route("/download/<path:output_file>")
-#def serve_download(filename):
+# def serve_download(filename):
 #    filename = os.path.basename(filename)
-    # print(filename) # 1.webm
-    # filename = os.path.join(BASE_DIR, os.path.basename(filename))
-    # print(filename)
-    # file_path = os.path.join(BASE_DIR, filename)
+# print(filename) # 1.webm
+# filename = os.path.join(BASE_DIR, os.path.basename(filename))
+# print(filename)
+# file_path = os.path.join(BASE_DIR, filename)
 
-    # return send_from_directory(file_path, filename, as_attachment=True)
-    # return send_from_directory("downloads", output_file, as_attachment=True)
+# return send_from_directory(file_path, filename, as_attachment=True)
+# return send_from_directory("downloads", output_file, as_attachment=True)
 #    return send_from_directory(BASE_DIR, filename, as_attachment=True)
