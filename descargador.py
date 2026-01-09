451a9237a653182f3fb9f557d7f15b9039bde8cb
andresok10 <davexgarcia@gmail.com>
2026-01-09 14:13:02 -0500

    ok

--- descargador.py
+++ descargador.py
@@ -211,12 +211,6 @@ def descargax():
             _external=True,
             _scheme="https",
         )
-
-        for archivo in os.listdir(CARPETA_DESCARGA):
-            print(f"📂 Contenido actual de CARPETA_DESCARGA {archivo}:")
-            ruta_completa = os.path.join(CARPETA_DESCARGA, archivo)
-            print(ruta_completa)
-            print("   ➜", archivo, end="\n")
     
         mime_type = "audio/mp4" if extension == "m4a" else "video/webm"
         return jsonify({
