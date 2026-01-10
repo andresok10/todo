2ec806940aa0d551c0a0b28ca5f2197c5fb4f485
andresok10 <davexgarcia@gmail.com>
2026-01-10 10:03:11 -0500

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,44 @@
-const CACHE = "web1-v1";
+/*const CACHE = "web1-v1";
 const OFFLINE_FILES = [
     "/",
-    "/index.html",
-    "/assets/app.js",
-    "/assets/style.css"
+    "/app.html",
 ];
 
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES))
     );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});*/
+
+const CACHE_NAME = "todo-v1";
+const OFFLINE_FILES = [
+    "/",                // ruta principal
+    "/app.html",
+    //"/app.py",
+    //"/assets/app.js",
+    //"/assets/style.css"
+    //"/static/style.css" // tu CSS real
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_FILES)).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
